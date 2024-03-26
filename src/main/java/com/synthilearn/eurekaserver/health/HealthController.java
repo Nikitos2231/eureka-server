92c3e0a9ecacf5b4ede6e74225923a8d1ca448e4
Nikitos2231 <nik.porshennikov@mail.ru>
2024-03-26 22:01:42 +0700

    Added workflows and health check

---
+++ src/main/java/com/synthilearn/eurekaserver/health/HealthController.java
@@ -0,0 +1,14 @@
+package com.synthilearn.eurekaserver.health;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public ResponseEntity<HealthResponse> checkHealth() {
+        return ResponseEntity.ok(new HealthResponse("OK"));
+    }
+}
---
+++ src/main/java/com/synthilearn/eurekaserver/health/HealthResponse.java
@@ -0,0 +1,18 @@
+package com.synthilearn.eurekaserver.health;
+
+public class HealthResponse {
+
+    private String status;
+
+    public HealthResponse(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
